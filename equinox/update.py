c1e4f7a05f4ae2b839df73a84283c1763548d8a3
callumtilbury <callum@tilbury.co.za>
2022-06-13 14:30:25 +0100

    Incremental update for a filtered PyTree.

--- equinox/update.py
+++ equinox/update.py
@@ -9,6 +9,11 @@ def _apply_update(u, p):
     else:
         return p + u
 
+def _incremental_update(old, new, step_size):
+    if new is None:
+        return old
+    else:
+        return (1 - step_size) * old + step_size * new
 
 def _is_none(x):
     return x is None
@@ -35,3 +40,14 @@ def apply_updates(model: PyTree, updates: PyTree) -> PyTree:
     """
     # Assumes that updates is a prefix of model
     return jax.tree_map(_apply_update, updates, model, is_leaf=_is_none)
+
+def incremental_update(new_model: PyTree, old_model: PyTree, step_size: float) -> PyTree:
+    """
+        TODO
+    """
+    return jax.tree_map(
+        lambda old, new : _incremental_update(old, new, step_size),
+        old_model,
+        new_model,
+        is_leaf=_is_none
+    )
